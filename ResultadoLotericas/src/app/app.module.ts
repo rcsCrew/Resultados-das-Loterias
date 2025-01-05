9d8ae54779a1963917bbb4f1cf1a9ffec4cc3d79
ramon senger <51542841+rcsCrew@users.noreply.github.com>
2025-01-05 23:49:14 +0000

    att 1/5 front 100%

---
+++ ResultadoLotericas/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HubComponent } from './pages/hub/hub.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HubComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ResultadoLotericas/src/app/pages/hub/hub.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hub',
+  standalone: false,
+  
+  templateUrl: './hub.component.html',
+  styleUrl: './hub.component.scss'
+})
+export class HubComponent {
+
+}
